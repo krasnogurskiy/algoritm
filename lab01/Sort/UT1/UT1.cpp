91a26551352bd100afb317966cc121940131277f
BoshkaS <bozena.salnikova@gmail.com>
2022-03-14 17:50:03 +0200

    Commit message

--- lab01/Sort/UT1/UT1.cpp
+++ lab01/Sort/UT1/UT1.cpp
@@ -10,46 +10,54 @@ namespace UT1
 	{
 	public:
 		
-		TEST_METHOD(TestBubbleSort)
+		TEST_METHOD(TestBubbleSort1)
 		{
 			const int Size = 10;
 			int Arr[Size] = { 2, 8, 5, 1, 20, 6, 4, 15, 9, 7 };
+			Assert::AreEqual(Arr[4], 20);
+			
 			BublleSort(Arr, Size);
-
-			Assert::AreEqual(Arr[4], 6);
 			Assert::AreEqual(Arr[7], 9);
-			Assert::AreEqual(Arr[9], 20);
+			
+			ReverseSortArray(Arr, Size);
+			Assert::AreEqual(Arr[9], 1);
 		}
 
 
 		TEST_METHOD(TestQuickSort) {
 			const int Size = 10;
 			int Arr[Size] = { 2, 8, 5, 1, 20, 6, 4, 15, 9, 7 };
-			QuickSort(Arr, 0, Size - 1);
+			Assert::AreEqual(Arr[4], 20);
 
-			Assert::AreEqual(Arr[4], 6);
+			QuickSort(Arr, 0, Size - 1);
 			Assert::AreEqual(Arr[7], 9);
-			Assert::AreEqual(Arr[9], 20);
+
+			ReverseSortArray(Arr, Size);
+			Assert::AreEqual(Arr[9], 1);
 		}
 
 		TEST_METHOD(TestShellSort) {
 			const int Size = 10;
 			int Arr[Size] = { 2, 8, 5, 1, 20, 6, 4, 15, 9, 7 };
-			ShellSort(Arr, Size);
+			Assert::AreEqual(Arr[4], 20);
 
-			Assert::AreEqual(Arr[4], 6);
+			ShellSort(Arr, Size);
 			Assert::AreEqual(Arr[7], 9);
-			Assert::AreEqual(Arr[9], 20);
+
+			ReverseSortArray(Arr, Size);
+			Assert::AreEqual(Arr[9], 1);
 		}
 
 		TEST_METHOD(TestRadixSort) {
 			const int Size = 10;
 			int Arr[Size] = { 2, 8, 5, 1, 20, 6, 4, 15, 9, 7 };
-			RadixSort(Arr, Size);
+			Assert::AreEqual(Arr[4], 20);
 
-			Assert::AreEqual(Arr[4], 6);
+			RadixSort(Arr, Size);
 			Assert::AreEqual(Arr[7], 9);
-			Assert::AreEqual(Arr[9], 20);
+
+			ReverseSortArray(Arr, Size);
+			Assert::AreEqual(Arr[9], 1);
 		}
 	};
 }
